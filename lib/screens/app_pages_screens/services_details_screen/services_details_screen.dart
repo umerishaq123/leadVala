67f667ffaa9c54faad6ced4f3b22fa8dd1d0d320
Mostafejur Rahman <mostafejur.smuct.cse@gmail.com>
2024-12-31 20:45:23 +0600

    Remove commented-out FAQ section from ServicesDetailsScreen

--- lib/screens/app_pages_screens/services_details_screen/services_details_screen.dart
+++ lib/screens/app_pages_screens/services_details_screen/services_details_screen.dart
@@ -87,62 +87,62 @@ class ServicesDetailsScreen extends StatelessWidget {
                                           ]).paddingSymmetric(vertical: Insets.i15),
                                           ServiceDescription(services: serviceCtrl.service),
                                         ]).paddingSymmetric(horizontal: Insets.i20),
-                                        if (serviceCtrl.serviceFaq.isNotEmpty)
-                                          Column(
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                            children: [
-                                              const VSpace(Sizes.s10),
-                                              Text(language(context, appFonts.faq),
-                                                      overflow: TextOverflow.clip,
-                                                      style: appCss.dmDenseBold16.textColor(appColor(context).darkText))
-                                                  .paddingSymmetric(horizontal: Sizes.s20),
-                                              const VSpace(Sizes.s10),
-                                              ...serviceCtrl.serviceFaq.asMap().entries.map((e) => Container(
-                                                    margin: const EdgeInsets.symmetric(
-                                                        horizontal: Sizes.s20, vertical: Sizes.s8),
-                                                    padding: const EdgeInsets.symmetric(horizontal: Sizes.s20),
-                                                    decoration: ShapeDecoration(
-                                                        shadows: [
-                                                          BoxShadow(
-                                                              blurRadius: 2,
-                                                              spreadRadius: 2,
-                                                              color: appColor(context).darkText.withOpacity(0.06))
-                                                        ],
-                                                        color: appColor(context).whiteBg,
-                                                        shape: SmoothRectangleBorder(
-                                                            borderRadius: SmoothBorderRadius(
-                                                                cornerRadius: 8, cornerSmoothing: 1))),
-                                                    child: ExpansionTile(
-                                                        expansionAnimationStyle:
-                                                            AnimationStyle(curve: Curves.fastOutSlowIn),
-                                                        key: Key(serviceCtrl.selected.toString()),
-                                                        initiallyExpanded: e.key == serviceCtrl.selected,
-                                                        onExpansionChanged: (newState) =>
-                                                            serviceCtrl.onExpansionChange(newState, e.key),
-                                                        //atten
-                                                        tilePadding: EdgeInsets.zero,
-                                                        collapsedIconColor: appColor(context).darkText,
-                                                        dense: true,
-                                                        iconColor: appColor(context).darkText,
-                                                        title: Text(e.value.question!,
-                                                            style: appCss.dmDenseMedium14
-                                                                .textColor(appColor(context).darkText)),
-                                                        children: <Widget>[
-                                                          Divider(
-                                                            color: appColor(context).stroke,
-                                                            height: .5,
-                                                            thickness: 0,
-                                                          ),
-                                                          ListTile(
-                                                              contentPadding:
-                                                                  const EdgeInsets.symmetric(horizontal: Sizes.s5),
-                                                              title: Text(e.value.answer!,
-                                                                  style: appCss.dmDenseLight14.textColor(
-                                                                      appColor(context).darkText.withOpacity(.8))))
-                                                        ]),
-                                                  ))
-                                            ],
-                                          ).marginOnly(top: Sizes.s10),
+                                        // if (serviceCtrl.serviceFaq.isNotEmpty)
+                                        //   Column(
+                                        //     crossAxisAlignment: CrossAxisAlignment.start,
+                                        //     children: [
+                                        //       const VSpace(Sizes.s10),
+                                        //       Text(language(context, appFonts.faq),
+                                        //               overflow: TextOverflow.clip,
+                                        //               style: appCss.dmDenseBold16.textColor(appColor(context).darkText))
+                                        //           .paddingSymmetric(horizontal: Sizes.s20),
+                                        //       const VSpace(Sizes.s10),
+                                        //       ...serviceCtrl.serviceFaq.asMap().entries.map((e) => Container(
+                                        //             margin: const EdgeInsets.symmetric(
+                                        //                 horizontal: Sizes.s20, vertical: Sizes.s8),
+                                        //             padding: const EdgeInsets.symmetric(horizontal: Sizes.s20),
+                                        //             decoration: ShapeDecoration(
+                                        //                 shadows: [
+                                        //                   BoxShadow(
+                                        //                       blurRadius: 2,
+                                        //                       spreadRadius: 2,
+                                        //                       color: appColor(context).darkText.withOpacity(0.06))
+                                        //                 ],
+                                        //                 color: appColor(context).whiteBg,
+                                        //                 shape: SmoothRectangleBorder(
+                                        //                     borderRadius: SmoothBorderRadius(
+                                        //                         cornerRadius: 8, cornerSmoothing: 1))),
+                                        //             child: ExpansionTile(
+                                        //                 expansionAnimationStyle:
+                                        //                     AnimationStyle(curve: Curves.fastOutSlowIn),
+                                        //                 key: Key(serviceCtrl.selected.toString()),
+                                        //                 initiallyExpanded: e.key == serviceCtrl.selected,
+                                        //                 onExpansionChanged: (newState) =>
+                                        //                     serviceCtrl.onExpansionChange(newState, e.key),
+                                        //                 //atten
+                                        //                 tilePadding: EdgeInsets.zero,
+                                        //                 collapsedIconColor: appColor(context).darkText,
+                                        //                 dense: true,
+                                        //                 iconColor: appColor(context).darkText,
+                                        //                 title: Text(e.value.question!,
+                                        //                     style: appCss.dmDenseMedium14
+                                        //                         .textColor(appColor(context).darkText)),
+                                        //                 children: <Widget>[
+                                        //                   Divider(
+                                        //                     color: appColor(context).stroke,
+                                        //                     height: .5,
+                                        //                     thickness: 0,
+                                        //                   ),
+                                        //                   ListTile(
+                                        //                       contentPadding:
+                                        //                           const EdgeInsets.symmetric(horizontal: Sizes.s5),
+                                        //                       title: Text(e.value.answer!,
+                                        //                           style: appCss.dmDenseLight14.textColor(
+                                        //                               appColor(context).darkText.withOpacity(.8))))
+                                        //                 ]),
+                                        //           ))
+                                        //     ],
+                                        //   ).marginOnly(top: Sizes.s10),
                                         if (serviceCtrl.service!.relatedServices != null &&
                                             serviceCtrl.service!.relatedServices!.isNotEmpty)
                                           HeadingRowCommon(
